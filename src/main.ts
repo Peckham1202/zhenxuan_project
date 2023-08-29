843c32b2d9a3fce6df52d7b53cf5f21918f61337
Peckham1202 <pichamli@163.com>
2023-08-29 20:38:17 +0800

    - 添加 src路径别名； - 添加 svg用的插件； - 完善 main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,23 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+//引入 element-plus 模块
+import ElementPlus from 'element-plus';
+//引入 element-plus 提供的样式
+import 'element-plus/dist/index.css';
+
+//@ts-expect-error 忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
+
+import 'virtual:svg-icons-register';
+
+//创建实例
+const app = createApp(App);
+
+//使用 element-plus 插件
+app.use(ElementPlus, {
+  locale: zhCn,
+});
+
+//挂载应用
+app.mount('#app');
